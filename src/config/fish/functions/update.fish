8a3d058f35a19637681b89878ecd8964164ac8e9
Marvin ROGER <dev@marvinroger.fr>
2019-10-23 23:02:41 +0200

    feat: add git GPG signature
    Signed-off-by: Marvin ROGER <dev@marvinroger.fr>

--- src/config/fish/functions/update.fish
+++ src/config/fish/functions/update.fish
@@ -1,13 +1,15 @@
 # Usage: update
 
-function update --description "Update homebrew programs"
-  pretty_log info "Starting update..."
+function update --description "Update programs"
+  pretty_log info "Starting homebrew update..."
   brew update
-  pretty_log info "Starting upgrade..."
+  pretty_log info "Starting homebrew upgrade..."
   brew upgrade
-  pretty_log info "Starting cask upgrade..."
+  pretty_log info "Starting homebrew cask upgrade..."
   brew cask upgrade
-  pretty_log info "Starting cleanup..."
+  pretty_log info "Starting homebrew cleanup..."
   brew cleanup
+  pretty_log info "Starting Mac App Store upgrade..."
+  mas upgrade
   pretty_log success "Done."
 end
