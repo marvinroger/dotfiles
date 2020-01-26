2701828100ff6f6dbdb183069c90581add092c24
Marvin ROGER <bonjour+git@marvinroger.fr>
2020-01-26 12:13:42 +0100

    feat: enhance time machine exclusion script

--- src/config/fish/functions/time_machine_exclude_deps.fish
+++ src/config/fish/functions/time_machine_exclude_deps.fish
@@ -1,6 +1,8 @@
 # Usage: time_machine_exclude_deps
 
 function time_machine_exclude_deps --description "Exclude dependencies from Time Machine backups"
+  pretty_log info "Excluding download dir"
+  tmutil addexclusion ~/Downloads
   pretty_log info "Excluding node_modules"
-  find (pwd) -type d -maxdepth 4 -name 'node_modules' -prune -exec tmutil addexclusion {} \;
+  find ~/Dev -type d -maxdepth 4 -name 'node_modules' -prune -exec tmutil addexclusion {} \;
 end
