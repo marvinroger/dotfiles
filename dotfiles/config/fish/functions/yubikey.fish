0832840716748c0708dc799463d6fa2ebd80e839
Marvin ROGER <bonjour+git@marvinroger.fr>
2020-04-03 21:28:18 +0200

    feat: install fonts and yubikey workaround

--- dotfiles/config/fish/functions/yubikey.fish
+++ dotfiles/config/fish/functions/yubikey.fish
@@ -1,10 +1,6 @@
 # Usage: yubikey <load|unload>
 
 function yubikey --description "Load/unload YubiKey into ssh-agent"
-  # Workaround: SSH does not work with symbolic links
-  # So we make a hard copy
-  rsync --checksum --copy-links /usr/local/lib/libykcs11.dylib /usr/local/lib/libykcs11_NOTALINK.dylib
-
   set --local argc (count $argv)
   if test $argc -ne 1
     pretty_log error "Please give load or unload."
